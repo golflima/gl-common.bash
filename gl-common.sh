063927e98674e93baa0b6fbec72f307a81157bf8
Jérémy WALTHER <jeremy.walther@golflima.net>
2017-02-16 00:26:30 +0100

    Remove background colors in remove_color()

--- gl-common.sh
+++ gl-common.sh
@@ -110,7 +110,8 @@ assertok() { ! $1 && warn "${LIGHT_RED}fatal: $(echo gl_common_get_var NAME) v$(
 remove_color() {
     NC=; BLACK=; DARK_GRAY=; RED=; LIGHT_RED=; GREEN=; LIGHT_GREEN=;
     BROWN=; YELLOW=; BLUE=; LIGHT_BLUE=; PURPLE=; LIGHT_PURPLE=;
-    CYAN=; LIGHT_CYAN=; LIGHT_GRAY=; WHITE=;
+    CYAN=; LIGHT_CYAN=; LIGHT_GRAY=; WHITE=; BG_BLACK=; BG_RED=;
+    BG_GREEN=; BG_BROWN=; BG_BLUE=; BG_PURPLE=; BG_CYAN=; BG_LIGHT_GRAY=;
 }
 
 # Make a spinner at the beginning of the standard output line
